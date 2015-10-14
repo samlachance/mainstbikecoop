4a27a779207d9501f6da6be68ce8e6ce1d00e8b6
Sam Lachance <slachance@gmail.com>
2015-10-14 18:00:10 -0400

    Simplified code

--- js/hours.js
+++ js/hours.js
@@ -1,12 +1,9 @@
+// Asks Javascript to figure out the day of the week and then assign that number value to weekDay. Sunday is 0, Monday is 1, and so on.
 var date = new Date();
 var weekDay = date.getDay();
 
-
-if (weekDay == 2) {
-  document.getElementById("hoursTop").textContent="Open today: 3pm - 10pm";
-}
-
-else if (weekDay == 4) {
+// Correlates the number day to the business's store hours.
+if (weekDay == 2 || weekDay == 4) {
   document.getElementById("hoursTop").textContent="Open today: 3pm - 10pm";
 }
 
